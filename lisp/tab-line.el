aef397b73b701b1aebed01acad7dbf8a19b85042
Juri Linkov <juri@linkov.net>
2020-02-27 00:37:39 +0200

    * lisp/tab-line.el: Add debugging diagnostics

--- lisp/tab-line.el
+++ lisp/tab-line.el
@@ -261,7 +261,7 @@ Append ellipsis `tab-line-tab-name-ellipsis' in this case."
                   'help-echo tab-name))))
 
 
-(defcustom tab-line-tabs-function #'tab-line-tabs-window-buffers
+(defcustom tab-line-tabs-function #'tab-line-tabs-mode-buffers
   "Function to get a list of tabs to display in the tab line.
 This function should return either a list of buffers whose names will
 be displayed, or just a list of strings to display in the tab line.
@@ -487,12 +487,15 @@ the selected tab visible."
   :group 'tab-line
   :version "27.1")
 
+(defvar tab-line-auto-hscroll-buffer (generate-new-buffer " *tab-line-hscroll*"))
+
 (defun tab-line-auto-hscroll (strings hscroll)
-  (with-temp-buffer
+  (with-current-buffer tab-line-auto-hscroll-buffer
     (let ((truncate-partial-width-windows nil)
           (inhibit-modification-hooks t)
           show-arrows)
       (setq truncate-lines nil)
+      (erase-buffer)
       (apply 'insert strings)
       (goto-char (point-min))
       (add-face-text-property (point-min) (point-max) 'tab-line)
@@ -559,6 +562,17 @@ the selected tab visible."
                     (setq hscroll (float new-hscroll))
                     (set-window-parameter nil 'tab-line-hscroll hscroll)))))))))
       (list show-arrows hscroll))))
+
+(defun test1 ()
+  (interactive)
+  (with-selected-window (split-window-below -10)
+    (switch-to-buffer "*Messages*")
+    (tab-line-mode 0))
+  (message "tab-line-hscroll: %S" (window-parameter nil 'tab-line-hscroll))
+  (message "tab-line-cache: %S" (cdr (car (window-parameter nil 'tab-line-cache))))
+  (tab-line-format-template (funcall tab-line-tabs-function))
+  (select-window (split-window-below -5))
+  (switch-to-buffer tab-line-auto-hscroll-buffer))
 
 
 (defun tab-line-hscroll (&optional arg window)
