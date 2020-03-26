9da6a04e293d51388b019d45f8f1ba626e39d55c
Juri Linkov <juri@linkov.net>
2020-03-27 00:09:36 +0200

    * diff-mode.el (diff-syntax-fontify-props): Set enable-local-variables to nil.

--- lisp/vc/diff-mode.el
+++ lisp/vc/diff-mode.el
@@ -2720,11 +2720,13 @@ hunk text is not found in the source file."
     ;; When initialization is requested, we should be in a brand new
     ;; temp buffer.
     (cl-assert (null buffer-file-name))
-    (let ((enable-local-variables :safe) ;; to find `mode:'
+    ;; Use `:safe' to find `mode:'.  In case of hunk-only, use nil because
+    ;; Local Variables list might be incomplete when context is truncated.
+    (let ((enable-local-variables (unless hunk-only :safe))
           (buffer-file-name file))
       ;; Don't run hooks that might assume buffer-file-name
       ;; really associates buffer with a file (bug#39190).
-      (delay-mode-hooks (ignore-errors (set-auto-mode)))
+      (delay-mode-hooks (set-auto-mode))
       ;; FIXME: Is this really worth the trouble?
       (when (and (fboundp 'generic-mode-find-file-hook)
                  (memq #'generic-mode-find-file-hook
