7e314ab60070b5561b302036f75583fccdf2b173
Juri Linkov <juri@linkov.net>
2020-10-21 23:11:28 +0300

    Add generate-x-compose-keymap

--- dev/x-compose.el
+++ dev/x-compose.el
@@ -4,31 +4,42 @@
 
 ;;; Code:
 
-(defvar x-compose-keymap
-  (let ((map (make-sparse-keymap)))
-    (let ((keysymdef (make-hash-table :test 'equal)))
-      (with-temp-buffer
-        (insert-file-contents "/usr/include/X11/keysymdef.h")
-        (while (re-search-forward "^#define XK_\\(\\S-+\\).+/\\* U\\+\\(\\w+\\)" nil t)
-          (puthash (match-string 1) (match-string 2) keysymdef)))
-      (with-temp-buffer
-        (insert-file-contents "/usr/share/X11/locale/en_US.UTF-8/Compose")
-        (while (re-search-forward "^<Multi_key> \\([^:]+\\): \"\\([^\"]+\\)\"" nil t)
-          (let* ((to-char (string-to-char (match-string 2)))
-                 (from-keys (match-string 1))
-                 (from-chars
-                  (mapcar (lambda (s)
-                            (if (string-match "^U\\([[:xdigit:]]+\\)" s)
-                                (string-to-number (match-string 1 s) 16)
-                              (string-to-number (gethash s keysymdef "0000") 16)))
-                          (split-string from-keys "[<> \t]+" t))))
-            (unless (memq 0 from-chars)
-              (define-key map (apply 'vector from-chars) (vector to-char)))))))
-    map)
-  "Keymap for keys of the Compose input method.")
+(defun generate-x-compose-keymap ()
+  (interactive)
+  (let ((map (make-sparse-keymap))
+        (keysymdef (make-hash-table :test 'equal))
+        x-compose-char-map errors)
+    (dolist (i '(("KP_Space" . kp-space) ("KP_Add" . kp-add) ("KP_Divide" . kp-divide) ("KP_Equal" . kp-equal)
+                 ("KP_0" . kp-0) ("KP_1" . kp-1) ("KP_2" . kp-2) ("KP_3" . kp-3) ("KP_4" . kp-4)
+                 ("KP_5" . kp-5) ("KP_6" . kp-6) ("KP_7" . kp-7) ("KP_8" . kp-8) ("KP_9" . kp-9)
+                 ))
+      (puthash (car i) (cdr i) keysymdef))
+    (with-temp-buffer
+      (insert-file-contents "/usr/include/X11/keysymdef.h")
+      (while (re-search-forward "^#define XK_\\(\\S-+\\).+/\\*[ (]U\\+\\([[:xdigit:]]+\\)" nil t)
+        (puthash (match-string 1) (string-to-number (match-string 2) 16) keysymdef)))
+    (with-temp-buffer
+      (insert-file-contents "/usr/share/X11/locale/en_US.UTF-8/Compose")
+      (while (re-search-forward "^<Multi_key> \\([^:]+\\): \"\\([^\"]+\\)\"" nil t)
+        ;; (when (> (length (match-string 2)) 1)
+        ;;   (push (match-string 2) errors))
+        (let* ((to-char (string-to-char (match-string 2)))
+               (from-keys (match-string 1))
+               (from-chars
+                (mapcar (lambda (s)
+                          (if (string-match "^U\\([[:xdigit:]]+\\)" s)
+                              (string-to-number (match-string 1 s) 16)
+                            (gethash s keysymdef -1)))
+                        (split-string from-keys "[<> \t]+" t))))
+          (if (memq -1 from-chars)
+              (push from-keys errors)
+            (push (cons (apply 'vector from-chars) (vector to-char))
+                  x-compose-char-map)))))
+    ;; (insert (pp-to-string errors))
+    (insert (pp-to-string x-compose-char-map))))
 
-(define-key key-translation-map [(control ?+)] x-compose-keymap)
-(global-set-key [(control ?+) (control ?+)] 'insert-char)
+;; (define-key key-translation-map [(control ?+)] x-compose-keymap)
+;; (global-set-key [(control ?+) (control ?+)] 'insert-char)
 
 (provide 'x-compose)
 ;;; x-compose.el ends here
