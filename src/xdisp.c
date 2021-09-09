b9a7545a3eb6b6ed5caf9b77d7ea03674b8f0381
Juri Linkov <juri@linkov.net>
2021-09-09 10:59:05 +0300

    Fix event-start/event-end

--- src/xdisp.c
+++ src/xdisp.c
@@ -12223,6 +12223,7 @@ clear_message (bool current_p, bool last_displayed_p)
       message_cleared_p = true;
 
       /* Before release obsolete ignoring return value in NEWS for some bug# */
+      /* ../../patch/clear-message.patch */
       if (FUNCTIONP (Vclear_message_function))
         {
           ptrdiff_t count = SPECPDL_INDEX ();
