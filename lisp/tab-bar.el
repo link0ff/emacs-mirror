c476e2b9d36b6f89960298ce424cf5683b0b0068
Juri Linkov <juri@linkov.net>
2021-09-12 18:07:37 +0300

    Change tab-bar-button-margin to 1

--- lisp/tab-bar.el
+++ lisp/tab-bar.el
@@ -161,7 +161,7 @@ For easier selection of tabs by their numbers, consider customizing
     (add-text-properties 0 (length tab-bar-new-button)
                          `(display (image :type xpm
                                           :file "tabs/new.xpm"
-                                          :margin ,(cons tab-bar-button-margin 0)
+                                          :margin ,tab-bar-button-margin
                                           :ascent center))
                          tab-bar-new-button))
 
@@ -171,7 +171,7 @@ For easier selection of tabs by their numbers, consider customizing
     (add-text-properties 0 (length tab-bar-close-button)
                          `(display (image :type xpm
                                           :file "tabs/close.xpm"
-                                          :margin ,(cons tab-bar-button-margin 0)
+                                          :margin ,tab-bar-button-margin
                                           :ascent center))
                          tab-bar-close-button)))
 
@@ -1662,7 +1662,7 @@ and can restore them."
           (add-text-properties 0 (length tab-bar-back-button)
                                `(display (image :type xpm
                                                 :file "tabs/left-arrow.xpm"
-                                                :margin ,(cons tab-bar-button-margin 0)
+                                                :margin ,tab-bar-button-margin
                                                 :ascent center))
                                tab-bar-back-button))
         (when (and tab-bar-mode (not (get-text-property 0 'display tab-bar-forward-button)))
@@ -1670,7 +1670,7 @@ and can restore them."
           (add-text-properties 0 (length tab-bar-forward-button)
                                `(display (image :type xpm
                                                 :file "tabs/right-arrow.xpm"
-                                                :margin ,(cons tab-bar-button-margin 0)
+                                                :margin ,tab-bar-button-margin
                                                 :ascent center))
                                tab-bar-forward-button))
 
