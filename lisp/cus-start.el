58885faa8ba6559b5d39729a5973644ca63a10ee
Juri Linkov <juri@linkov.net>
2021-12-06 17:14:06 +0200

    Add set-message-functions

--- lisp/cus-start.el
+++ lisp/cus-start.el
@@ -675,16 +675,7 @@ since it could result in memory overflow and make Emacs crash."
 						     :format "%v")
 					    (other :tag "Unlimited" t))
 			      "24.3")
-             (set-message-function
-              minibuffer (choice
-                          (const :tag "No special handling" nil)
-                          (const :tag "Handle minibuffer" set-minibuffer-message)
-                          (const :tag "Accumulate messages" set-multi-message)
-                          (const :tag "Accumulate messages and handle minibuffer"
-                                 set-minibuffer-multi-message)
-                          (function :tag "Other function"))
-              "28.1")
-	     (unibyte-display-via-language-environment mule boolean)
+             (unibyte-display-via-language-environment mule boolean)
 	     (blink-cursor-alist cursor alist "22.1")
 	     (overline-margin display integer "22.1")
 	     (underline-minimum-offset display integer "23.1")
