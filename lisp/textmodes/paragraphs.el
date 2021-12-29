e6b09963f626f04975b6f9e803373732f3e06be0
Juri Linkov <juri@linkov.net>
2021-12-29 21:10:50 +0200

    * lisp/textmodes/paragraphs.el (repunctuate-sentences-filter): New function.
    (repunctuate-sentences): Use it on isearch-filter-predicate
    to skip unnecessary matches.  (bug#52769)

--- lisp/textmodes/paragraphs.el
+++ lisp/textmodes/paragraphs.el
@@ -480,9 +480,12 @@ sentences.  Also, every paragraph boundary terminates sentences as well."
     (constrain-to-field nil opoint t)))
 
 (defun repunctuate-sentences-filter (_start _end)
-  ;; (message "! %S %S" (point) (buffer-substring-no-properties (- (point) 4) (point)))
-  (not (or (length= (match-string 4) 2)
-           (looking-back (rx (or "e.g." "i.e.") " ") 5))))
+  "Search filter used by `repunctuate-sentences' to skip unneeded spaces.
+By default, it skips occurrences that already have two spaces.
+It is advised to put `advice-add' on this function to add more filters,
+for example, `(looking-back (rx (or \"e.g.\" \"i.e.\") \" \") 5)'
+with a set of predefined abbreviations to skip from adding two spaces."
+  (not (length= (match-string 4) 2)))
 
 (defun repunctuate-sentences (&optional no-query start end)
   "Put two spaces at the end of sentences from point to the end of buffer.
@@ -494,14 +497,13 @@ asking for confirmation."
   (interactive (list nil
                      (if (use-region-p) (region-beginning))
                      (if (use-region-p) (region-end))))
-  (if no-query
-      (let ((regexp "\\([]\"')]?\\)\\([.?!]\\)\\([]\"')]?\\) +")
-            (to-string "\\1\\2\\3  "))
-        (when start (goto-char start))
-        (while (re-search-forward regexp end t)
-          (replace-match to-string)))
-    (let ((regexp "\\([]\"')]?\\)\\([.?!]\\)\\([]\"')]?\\)\\( +\\)")
-          (to-string "\\1\\2\\3  "))
+  (let ((regexp "\\([]\"')]?\\)\\([.?!]\\)\\([]\"')]?\\)\\( +\\)")
+        (to-string "\\1\\2\\3  "))
+    (if no-query
+        (progn
+          (when start (goto-char start))
+          (while (re-search-forward regexp end t)
+            (replace-match to-string)))
       (unwind-protect
           (progn
             (add-function :after-while isearch-filter-predicate
