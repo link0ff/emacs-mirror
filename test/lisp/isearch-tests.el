cd3869513b248c644613725a6ff4928ba310d35d
Juri Linkov <juri@linkov.net>
2022-06-29 18:36:11 +0300

    * test/lisp/isearch-tests.el: Add isearch--test-search-fun-in-text-property.

--- test/lisp/isearch-tests.el
+++ test/lisp/isearch-tests.el
@@ -38,5 +38,61 @@
   ;; Bug #21091: let `isearch-done' work without `isearch-update'.
   (isearch-done))
 
+
+;; Search functions.
+
+(ert-deftest isearch--test-search-fun-in-text-property ()
+  (let ((isearch-search-fun-function
+         (lambda () (isearch-search-fun-in-text-property nil 'dired-filename)))
+        (pairs '((4 . 7) (11 . 14) (21 . 24))))
+    (with-temp-buffer
+      (insert "foo" (propertize "foo" 'dired-filename t) "foo\n")
+      (insert (propertize "foo" 'dired-filename t) "foo\n")
+      (insert "foo" (propertize "foo" 'dired-filename t) "\n")
+
+      (goto-char (point-min))
+      (let ((isearch-forward t)
+            (isearch-regexp nil))
+        (dolist (pos (append pairs nil))
+          (should (eq (cdr pos) (isearch-search-string "foo" nil t)))
+          (when (car pos) (should (eq (car pos) (match-beginning 0))))))
+
+      (goto-char (point-max))
+      (let ((isearch-forward nil)
+            (isearch-regexp nil))
+        (dolist (pos (append (reverse pairs) nil))
+          (should (eq (car pos) (isearch-search-string "foo" nil t)))
+          (when (cdr pos) (should (eq (cdr pos) (match-end 0))))))
+
+      (goto-char (point-min))
+      (let ((isearch-forward t)
+            (isearch-regexp t))
+        (dolist (pos (append pairs nil))
+          (should (eq (cdr pos) (isearch-search-string ".*" nil t)))
+          (when (car pos) (should (eq (car pos) (match-beginning 0))))))
+
+      (goto-char (point-min))
+      (let ((isearch-forward t)
+            (isearch-regexp t))
+        (dolist (pos (append pairs nil))
+          (should (eq (cdr pos) (isearch-search-string "^.*" nil t)))
+          (when (car pos) (should (eq (car pos) (match-beginning 0))))))
+
+      (goto-char (point-min))
+      (let ((isearch-forward t)
+            (isearch-regexp t))
+        (dolist (pos (append pairs nil))
+          (should (eq (cdr pos) (isearch-search-string ".*$" nil t)))
+          (when (car pos) (should (eq (car pos) (match-beginning 0))))))
+
+      (goto-char (point-max))
+      (let ((isearch-forward nil)
+            (isearch-regexp t))
+        (dolist (pos (append (reverse pairs) nil))
+          (should (eq (car pos) (isearch-search-string "^.*" nil t)))
+          (when (cdr pos) (should (eq (cdr pos) (match-end 0)))))))))
+
+;; (message "! %S %S %S" (isearch-search-string ".*$" nil t) (match-beginning 0) (match-end 0))
+
 (provide 'isearch-tests)
 ;;; isearch-tests.el ends here
