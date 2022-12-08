a5832d192ef6d7e45da1598e9bd2e1482356d3d8
Juri Linkov <juri@linkov.net>
2022-12-08 18:05:18 +0200

    * lisp/replace.el (replace-highlight): Bind isearch-invisible search-invisible

--- lisp/replace.el
+++ lisp/replace.el
@@ -2750,6 +2750,7 @@ to a regexp that is actually used for the search.")
 	    (isearch-regexp-lax-whitespace
 	     replace-regexp-lax-whitespace)
 	    (isearch-case-fold-search case-fold)
+	    (isearch-invisible search-invisible)
 	    (isearch-forward (not backward))
 	    (isearch-other-end match-beg)
 	    (isearch-error nil)
